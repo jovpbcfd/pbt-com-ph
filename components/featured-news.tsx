aa69eb65c45a3dd8218808a53d7e4df50199519c
Jovan Lanutan <jovan.lanutan@cwti.local>
2025-04-07 10:58:29 +0800

    Disable news image for now

--- components/featured-news.tsx
+++ components/featured-news.tsx
@@ -2,11 +2,13 @@ export default function FeaturedNews() {
   return (
     <section className="max-w-[1230px] mx-auto text-white p-2 md:p-0">
       <div className="my-2 md:mt-10">
-        <h2 className="font-[900] text-lg md:text-xl lg:text-2xl">
+        {/* text-left */}
+        <h2 className="font-[900] text-center text-lg md:text-xl lg:text-2xl">
           Keep informed with Panalobet News
         </h2>
         <div className="flex items-center flex-col-reverse gap-2 mt-2 md:my-4 lg:justify-center lg:flex-row">
-          <div className="w-full text-justify md:w-[50%]">
+          {/* md:w-[50%] */}
+          <div className="w-full text-justify ">
             <p className="mb-2 md:mb-4">
               At Panalobet, we think that great customer care is mostly
               responsible for great gaming experience. Our committed customer
@@ -22,7 +24,7 @@ export default function FeaturedNews() {
               as it may be.
             </p>
           </div>
-          <div className="w-full grid grid-cols-1 md:gap-3 md:ml-10 md:rounded-md lg:grid-cols-2">
+          {/* <div className="w-full grid grid-cols-1 md:gap-3 md:ml-10 md:rounded-md lg:grid-cols-2">
             <div>
               <div className="border-1 border-dashed flex items-center justify-center rounded-md md:w-[364px] md:h-[203px]">
                 Image
@@ -47,7 +49,7 @@ export default function FeaturedNews() {
                 </p>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
     </section>
